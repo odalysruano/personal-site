8bd237dd7733b9e34e5345f153e7e049ed7226d4
odalysruano <odalys.r.ruano@gmail.com>
2024-08-26 15:20:11 -0400

    add template NavBar

--- my-app/src/pages/App/App.tsx
+++ my-app/src/pages/App/App.tsx
@@ -1,11 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
+import NavBar from '../../components/NavBar/NavBar';
+import './App.css';
 import MyHomePage from '../MyHomePage/MyHomePage';
 
 function App() {
   return (
     <ChakraProvider>
+      <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<MyHomePage />} />
       </Routes>
