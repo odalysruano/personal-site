d3afc8b8f8703cb2c640382c0eff7fff697bb2ab
odalysruano <odalys.r.ruano@gmail.com>
2024-08-26 17:11:34 -0400

    add footer

---
+++ my-app/src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+export default function Footer() {
+    return(
+        <>
+            <Box
+                bg='#9AE6B4'
+                px={4}
+                boxShadow="md"
+            >
+                <Flex h={10} alignItems={'center'} justifyContent={'flex-end'}>
+                    <Text>© 2024 Odalys Ruano. All Rights Reserved.</Text>
+                </Flex>
+            </Box>
+        </>
+    )
+}
--- my-app/src/pages/App/App.tsx
+++ my-app/src/pages/App/App.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
 import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 import MyHomePage from '../MyHomePage/MyHomePage';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MyHomePage />} />
       </Routes>
+      <Footer></Footer>
     </ChakraProvider>
   );
 }
