160aea417d21daef63e72dfc0abf32238387b4ed
odalysruano <odalys.r.ruano@gmail.com>
2024-08-25 20:57:52 -0400

    initialize MyHomePage

--- my-app/src/pages/App/App.tsx
+++ my-app/src/pages/App/App.tsx
@@ -1,26 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import logo from '../../logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import './App.css'
+import MyHomePage from '../MyHomePage/MyHomePage';
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Routes>
+        <Route path="/" element={<MyHomePage />} />
+      </Routes>
     </ChakraProvider>
   );
 }
--- my-app/src/pages/MyHomePage/MyHomePage.tsx
+++ my-app/src/pages/MyHomePage/MyHomePage.tsx
@@ -1,1 +1,9 @@
-export{}
+import { Box } from '@chakra-ui/react'
+
+export default function MyHomePage() {
+    return (
+        <Box>
+            Hi! My name is Odalys!
+        </Box>
+    )
+}
