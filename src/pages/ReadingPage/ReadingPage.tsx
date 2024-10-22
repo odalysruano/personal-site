b05ceae9962360ebb02c2d6e6d265c95730ee198
odalysruano <odalys.r.ruano@gmail.com>
2024-10-22 14:58:40 -0400

    create ReadingPage folder and file

---
+++ src/pages/ReadingPage/ReadingPage.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+export default function ReadingPage() {
+    return (
+        <Box textAlign='center' py='50px'>
+            <Heading size='xl'>My Reading List</Heading>
+            <Text pt='4'>Here is my list:</Text>
+        </Box>
+    );
+}
