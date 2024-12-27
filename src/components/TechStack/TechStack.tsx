327d76f9c8285a5a9a14741200a364d72fc07f59
odalysruano <odalys.r.ruano@gmail.com>
2024-12-26 19:12:41 -0500

    optimize TechStack component UI for mobile format

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -6,28 +6,63 @@ export default function TechStack() {
             display='flex'
             flexDirection='column'
             alignItems='center'
-            px={{ base: '50px', md: '200px' }} 
+            px={{ base: '20px', md: '200px' }} 
             py='5'
         >
-            <Heading mb='4' mt='4'size='xl'>What I Build With 🛠️</Heading>
+            <Heading mb='4' mt='4' size={{ base: 'lg', md: 'xl' }}>What I Build With 🛠️</Heading>
             <Box 
                 bg='white'
-                p={8}
-                px={{ base: '16px', md: '36px' }}
+                p={{ base: 4, md: 8 }}
+                px={{ base: '12px', md: '36px' }}
                 boxShadow='md'
                 borderRadius='md'
                 mb={6}
                 mx='auto'
-                maxWidth='700px'
+                maxWidth={{ base: '100%', md: '700px' }}
                 textAlign='left'
             >
-                <Text><b>Programming Languages:</b> Python, JavaScript/Typescript, HTML, CSS, SQL, Bash</Text>
-                <Text><b>Frameworks:</b> Django, React.js, Express.js, EJS, Node.js, Jquery</Text>
-                <Text><b>UI Frameworks:</b> Bootstrap, Materialize, Material UI, Chakra UI</Text>
-                <Text><b>Databases:</b> AWS RDS (PostgreSQL), MongoDB (NoSQL)</Text>
-                <Text><b>CI/CD:</b> AWS CodeBuild, AWS CodePipeline</Text>
-                <Text><b>Cloud:</b> AWS EC2, AWS Elastic Beanstalk, AWS IAM, AWS S3, AWS Route 53, AWS CloudFront, AWS Certificate Manager</Text>
-                <Text><b>Other:</b> REST APIs, JSON, Git, Jira, Confluence, Salesforce, Trello, Airtable, JWT, OAuth, SSL Certificates</Text>
+                <Text 
+                    fontSize={{ base: 'sm', md: 'md' }}
+                    mb={{ base: 3, md: 2 }}
+                >
+                    <b>Programming Languages:</b> Python, JavaScript/Typescript, HTML, CSS, SQL, Bash
+                </Text>
+                <Text 
+                    fontSize={{ base: 'sm', md: 'md' }}
+                    mb={{ base: 3, md: 2 }}
+                >
+                    <b>Frameworks:</b> Django, React.js, Express.js, EJS, Node.js, Jquery
+                </Text>
+                <Text 
+                    fontSize={{ base: 'sm', md: 'md' }}
+                    mb={{ base: 3, md: 2 }}
+                >
+                    <b>UI Frameworks:</b> Bootstrap, Materialize, Material UI, Chakra UI
+                </Text>
+                <Text 
+                    fontSize={{ base: 'sm', md: 'md' }}
+                    mb={{ base: 3, md: 2 }}
+                >
+                    <b>Databases:</b> AWS RDS (PostgreSQL), MongoDB (NoSQL)
+                </Text>
+                <Text 
+                    fontSize={{ base: 'sm', md: 'md' }}
+                    mb={{ base: 3, md: 2 }}
+                >
+                    <b>CI/CD:</b> AWS CodeBuild, AWS CodePipeline
+                </Text>
+                <Text 
+                    fontSize={{ base: 'sm', md: 'md' }}
+                    mb={{ base: 3, md: 2 }}
+                >
+                    <b>Cloud:</b> AWS EC2, AWS Elastic Beanstalk, AWS IAM, AWS S3, AWS Route 53, AWS CloudFront, AWS Certificate Manager
+                </Text>
+                <Text 
+                    fontSize={{ base: 'sm', md: 'md' }}
+                    mb={0}
+                >
+                    <b>Other:</b> REST APIs, JSON, Git, Jira, Confluence, Salesforce, Trello, Airtable, JWT, OAuth, SSL Certificates
+                </Text>
             </Box>
         </Box>
     );
