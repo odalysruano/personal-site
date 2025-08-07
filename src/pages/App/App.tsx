98fbdbe68297c4b905d3d38fa2455d5461eeadf7
odalysruano <odalys.r.ruano@gmail.com>
2025-08-07 22:56:49 +0200

    add memify URL to main router app

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -4,6 +4,7 @@ import Footer from '../../components/Footer/Footer';
 import HomePage from '../HomePage/HomePage';
 import NavBar from '../../components/NavBar/NavBar';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import MemifyPage from '../MemifyPage/MemifyPage';
 import MyCareerPage from '../MyCareerPage/MyCareerPage';
 import MyWorkPage from '../MyWorkPage/MyWorkPage';
 import ReadingPage from '../ReadingPage/ReadingPage';
@@ -22,6 +23,7 @@ function App() {
         <Box flex ="1">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/memify" element={<MemifyPage />} />
             <Route path="/career" element={<MyCareerPage />} />
             <Route path="/work" element={<MyWorkPage />} />
             <Route path="/reading2024" element={<ReadingPage props={reading24} />} />
