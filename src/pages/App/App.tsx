dcb2236f44c8a75747fc945b26cf17cf83cc16ac
odalysruano <odalys.r.ruano@gmail.com>
2025-08-07 16:27:51 +0200

    add URL for MyCareerPage to main router app

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,10 +1,10 @@
 import { ChakraProvider, Box } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
-import CareerPage from '../MyCareerPage/MyCareerPage';
 import Footer from '../../components/Footer/Footer';
 import HomePage from '../HomePage/HomePage';
 import NavBar from '../../components/NavBar/NavBar';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import MyCareerPage from '../MyCareerPage/MyCareerPage';
 import MyWorkPage from '../MyWorkPage/MyWorkPage';
 import ReadingPage from '../ReadingPage/ReadingPage';
 import ScrollToTop from '../../components/ScrollToTop/ScrollToTop';
@@ -22,7 +22,7 @@ function App() {
         <Box flex ="1">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/career" element={<CareerPage />} />
+            <Route path="/career" element={<MyCareerPage />} />
             <Route path="/work" element={<MyWorkPage />} />
             <Route path="/reading2024" element={<ReadingPage props={reading24} />} />
             <Route path="/reading2025" element={<ReadingPage props={reading25} />} />
