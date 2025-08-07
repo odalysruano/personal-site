118247de38ac1bb961680d22719eb24bffa67148
odalysruano <odalys.r.ruano@gmail.com>
2025-08-07 18:54:21 +0200

    add padding to top of heading and add text blurb to MyWorkPage

--- src/pages/MyWorkPage/MyWorkPage.tsx
+++ src/pages/MyWorkPage/MyWorkPage.tsx
@@ -1,17 +1,21 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Text, VStack } from '@chakra-ui/react';
 import Work from '../../components/Work/Work';
 import { projects } from '../../constants/projects';
 
 export default function MyWorkPage() {
     return (
-        // This container sets the background color and ensures the page has a minimum height
         <Box p={8} bg='#FAC589' minH='100vh'>
-            <Box maxW='1000px' mx='auto' textAlign='center'>
-                <Heading mb={4} size={{ base: 'lg', md: 'xl' }}>
+            <VStack spacing={10} maxW='1000px' mx='auto' textAlign='center' pt={8}>
+                <Heading as="h1" size="2xl" color="gray.800">
                     My Work 💡
                 </Heading>
+
+                <Text fontSize="lg" color="gray.600" maxW="800px">
+                    Below is a curated collection of my work, from foundational bootcamp projects to more complex, real-world applications. Each project represents a step in my journey as a developer, showcasing my passion for building intuitive, user-focused solutions and my ability to tackle challenges across the entire tech stack.
+                </Text>
+                
                 <Work projects={projects} />
-            </Box>
+            </VStack>
         </Box>
     );
 }
