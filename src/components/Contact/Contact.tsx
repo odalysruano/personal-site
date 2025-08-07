2698cf1754ad1de42e0b5ee1fff871ed713e61d5
odalysruano <odalys.r.ruano@gmail.com>
2025-08-07 22:08:02 +0200

    update Contact component to include location

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,6 @@
 import { Box, Heading, HStack, Icon, Link, Text } from '@chakra-ui/react';
 import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io';
+import { IoLocationSharp } from 'react-icons/io5';
 
 
 export default function Contact() {
@@ -10,6 +11,12 @@ export default function Contact() {
                 Interested in connecting? Whether you&apos;d like to learn more about my journey, discuss potential job opportunities, or simply network, I&apos;d love to hear from you. Feel free to reach out!
             </Text>
             <Text pt='2'>Email: odalys.r.ruano@gmail.com</Text>
+            
+            <HStack justifyContent='center' pt='2'>
+                <Icon as={IoLocationSharp} boxSize={6} />
+                <Text>New York, NY</Text>
+            </HStack>
+
             <HStack justifyContent='center' pt='2' spacing={4}>
                 <Link href='https://www.linkedin.com/in/odalysruano/' isExternal>
                     <Icon as={IoLogoLinkedin} boxSize={10} />
